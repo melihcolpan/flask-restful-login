d1cad7fe2f5a7cad36fa05cdc2e61dd1513ea783
melihcolpan <melihcolpan1@gmail.com>
2016-07-18 08:39:07 +0300

    Database create directory.

--- api/conf/config.py
+++ api/conf/config.py
@@ -4,4 +4,5 @@
 import os
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
+# Create a database in project and get it's path.
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
