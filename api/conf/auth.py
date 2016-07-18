90207a43b5f027f15fc1e75ec64d67c8504dc36c
melihcolpan <melihcolpan1@gmail.com>
2016-07-18 08:37:47 +0300

    JWT, refresh token and auth object creation.

--- api/conf/auth.py
+++ api/conf/auth.py
@@ -4,7 +4,11 @@
 from flask_httpauth import HTTPTokenAuth
 from itsdangerous import TimedJSONWebSignatureSerializer as JWT
 
+# JWT creation.
 jwt = JWT('top secret!', expires_in=3600)
+
+# Refresh token creation.
 refresh_jwt = JWT('telelelele', expires_in=7200)
 
+# Auth object creation.
 auth = HTTPTokenAuth('Bearer')
