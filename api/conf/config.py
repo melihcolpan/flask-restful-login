6b6cb91cf6aeaaab3063411b30426f248e458339
Melih Çolpan <melihcolpan1@gmail.com>
2020-12-27 05:09:47 +0300

    - PEP8 fixed - Index route added - Unauthorized error added

--- api/conf/config.py
+++ api/conf/config.py
@@ -3,7 +3,7 @@
 
 import os
 
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 # Create a database in project and get it's path.
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "test.db")
--- api/conf/routes.py
+++ api/conf/routes.py
@@ -3,10 +3,18 @@
 
 from flask_restful import Api
 
-from api.handlers.UserHandlers import (AddUser, DataAdminRequired,
-                                       DataUserRequired, Login, Logout,
-                                       RefreshToken, Register, ResetPassword,
-                                       UsersData)
+from api.handlers.UserHandlers import (
+    AddUser,
+    DataAdminRequired,
+    DataUserRequired,
+    Index,
+    Login,
+    Logout,
+    RefreshToken,
+    Register,
+    ResetPassword,
+    UsersData,
+)
 
 
 def generate_routes(app):
@@ -15,29 +23,32 @@ def generate_routes(app):
     api = Api(app)
 
     # Add all routes resources.
+    # Index page.
+    api.add_resource(Index, "/")
+
     # Register page.
-    api.add_resource(Register, '/v1/auth/register')
+    api.add_resource(Register, "/v1/auth/register")
 
     # Login page.
-    api.add_resource(Login, '/v1/auth/login')
+    api.add_resource(Login, "/v1/auth/login")
 
     # Logout page.
-    api.add_resource(Logout, '/v1/auth/logout')
+    api.add_resource(Logout, "/v1/auth/logout")
 
     # Refresh page.
-    api.add_resource(RefreshToken, '/v1/auth/refresh')
+    api.add_resource(RefreshToken, "/v1/auth/refresh")
 
     # Password reset page. Not forgot.
-    api.add_resource(ResetPassword, '/v1/auth/password_reset')
+    api.add_resource(ResetPassword, "/v1/auth/password_reset")
 
     # Get users page with admin permissions.
-    api.add_resource(UsersData, '/users')
+    api.add_resource(UsersData, "/users")
 
     # Example admin handler for admin permission.
-    api.add_resource(DataAdminRequired, '/data_admin')
+    api.add_resource(DataAdminRequired, "/data_admin")
 
     # Example user handler for user permission.
-    api.add_resource(DataUserRequired, '/data_user')
+    api.add_resource(DataUserRequired, "/data_user")
 
     # Example user handler for user permission.
-    api.add_resource(AddUser, '/user_add')
+    api.add_resource(AddUser, "/user_add")
